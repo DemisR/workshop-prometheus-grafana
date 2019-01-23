8ea54ba9968be757b9eb616e223e332501856797
Demis Rizzotto <demis.rizzotto@exxoss.com>
2019-01-23 14:54:48 +0100

    Add custom metric

--- example_springboot/spring-boot-management-example/src/main/java/com/example/springbootmanagementexample/SimpleRestController.java
+++ example_springboot/spring-boot-management-example/src/main/java/com/example/springbootmanagementexample/SimpleRestController.java
@@ -2,13 +2,18 @@ package com.example.springbootmanagementexample;
 
 import java.util.Date;
 
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Counter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
+	private Counter hellos = Metrics.counter("hellos");
+
 	@GetMapping("/example")
 	public String example() {
+		hellos.increment();
 		return "Hello User !! " + new Date();
 	}
 }
